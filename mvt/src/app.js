79ca136318db5b85aff830dd1f02d985ea0b0754
sk <crazycapivara@gmail.com>
2019-06-13 16:43:47 +0200

    This is where it all begins

---
+++ mvt/src/app.js
@@ -0,0 +1,62 @@
+import getTileData from "./get-tile-data";
+
+const LAT = 52.518728;
+const LNG = 13.395767;
+
+export class Map {
+    constructor(container, server, props) {
+        this._server = server;
+        this._createTooltipElement(container);
+        this._createWidget(container, props);
+    }
+
+    _createTooltipElement(container) {
+        const tooltip = document.createElement("div");
+        tooltip.style.cssText = "position: absolute; color: #fff; padding: 4px; background: rgba(0, 0, 0, 0.8); z-index: 9;";
+        document.getElementById(container).appendChild(tooltip);
+        this._tooltip = tooltip;
+    }
+
+    _createWidget(container, props) {
+        this._deck = new deck.DeckGL(Object.assign({
+            mapStyle: "",
+            container: container,
+            longitude: LNG,
+            latitude: LAT,
+            zoom: 10,
+            layers: this._renderLayers(),
+            pickingRadius: 5
+        }, props));
+    }
+
+    _renderLayers() {
+        return [
+            new deck.TileLayer({
+                lineWidthMinPixels: 1,
+                getLineWidth: this._getLineWidth,
+                getLineColor: this._getLineColor,
+                getTileData: getTileData(this._server),
+                pickable: true,
+                onHover: ({ object }) => {
+                    if (object) {
+                        this._tooltip.innerHTML = `${object.properties.name}`;
+                    } else { this._tooltip.innerHTML = ""; }
+                }
+            })
+        ];
+    }
+
+    _getLineColor(feature) {
+        const speed = feature.properties.speed;
+        return  speed < 40 ? [255, 0, 0] : speed < 60 ? [0, 0, 255] : [0, 240, 10];
+    }
+
+    _getLineWidth(feature) {
+        return feature.properties.name.startsWith("A ") ? 100 : 20;
+        // return feature.properties.speed < 60 ? 20 : 100;
+    }
+
+    setViewState(lat, lng, zoom) {
+        this._deck.setProps({ viewState: { latitude: lat, longitude: lng, zoom: zoom || 10 } });
+    }
+}
---
+++ mvt/src/get-tile-data.js
@@ -0,0 +1,25 @@
+import { VectorTile } from "@mapbox/vector-tile";
+import Protobuf from "pbf"; 
+
+export default function(server) {
+    return function({ x, y, z }) {
+        const mapSource = `${server}/mvt/${z}/${x}/${y}.mvt`;
+        return fetch(mapSource)
+        .then(response => response.arrayBuffer())
+        .then(buffer => {
+            const tile = new VectorTile(new Protobuf(buffer));
+            // console.log(tile);
+            const features = [];
+            for (const layerName in tile.layers) {
+                const vectorTileLayer = tile.layers[layerName];
+                for (let i = 0; i < vectorTileLayer.length; i++) {
+                    const vectorTileFeature = vectorTileLayer.feature(i);
+                    const feature = vectorTileFeature.toGeoJSON(x, y, z);
+                    features.push(feature);
+                }
+            }
+
+            return features;
+        });
+    };
+};
